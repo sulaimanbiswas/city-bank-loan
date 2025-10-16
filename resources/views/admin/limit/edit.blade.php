5bdd1addfa5140c0e6a5b524948e6d6990780e5c
Md Suliman <sulaimanbiswasbd@gmail.com>
2025-10-16 20:07:03 +0600

    feat: Update breadcrumb navigation in limit settings view

--- resources/views/admin/limit/edit.blade.php
+++ resources/views/admin/limit/edit.blade.php
@@ -13,6 +13,11 @@
                         class="relative text-slate-500 text-sm rtl:rotate-180"></iconify-icon>
                 </a>
             </li>
+            <li class="inline-block relative text-sm text-primary-500 font-Inter ">
+                Settings
+                <iconify-icon icon="heroicons-outline:chevron-right"
+                    class="relative top-[3px] text-slate-500 rtl:rotate-180"></iconify-icon>
+            </li>
             <li class="inline-block relative text-sm text-slate-500 font-Inter">Limit Settings</li>
         </ul>
     </div>
